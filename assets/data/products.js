5c96f50c498ac1e1405b4ddc2de61a81d0f3c9b6
Raihan Nur Daffa <nurdaffaraihan46@gmail.com>
2025-09-14 14:34:39 +0700

    add produk

--- assets/data/products.js
+++ assets/data/products.js
@@ -13,15 +13,63 @@ const products = [
   },
   {
     id: 3,
-    name: "Beras Pulen 1L",
+    name: "Beras Pulen 1 Liter ",
     price: 13000,
     image: "./assets/images/Beras_Pulen.jpg",
   },
   {
     id: 4,
+    name: "Beras Pulen 1/2 ",
+    price: 7000,
+    image: "./assets/images/Beras_Pulen.jpg",
+  },
+  {
+    id: 5,
     name: "Indomie Goreng",
     price: 4000,
     image: "./assets/images/Indomie_Goreng.jpeg",
+  },
+  {
+    id: 6,
+    name: "Kecap Nasional",
+    price: 14000,
+    image: "./assets/images/Kecap_Nasional.jpg",
+  },
+  {
+    id: 7,
+    name: "Kecap Nasional Botol",
+    price: 17000,
+    image: "./assets/images/Kecap_Nasional_Botol.jpg",
+  },
+  {
+    id: 8,
+    name: "Kecap Guna",
+    price: 8000,
+    image: "./assets/images/Kecap_Guna.jpg",
+  },
+  {
+    id: 9,
+    name: "Sasa 6k",
+    price: 6000,
+    image: "./assets/images/Sasa6k.jpg",
+  },
+  {
+    id: 10,
+    name: "Sasa 3k",
+    price: 3000,
+    image: "./assets/images/Sasa3k.jpg",
+  },
+  {
+    id: 11,
+    name: "Sasa 1k",
+    price: 1000,
+    image: "./assets/images/Sasa1k.jpeg",
+  },
+  {
+    id: 12,
+    name: "Sasa Besar",
+    price: 12000,
+    image: "./assets/images/Sasa.jpg",
   },
   // Tambahkan produk lain di sini
 ];
