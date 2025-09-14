835749e10517b456dca281e861bb9f02724052df
Raihan Nur Daffa <nurdaffaraihan46@gmail.com>
2025-09-14 13:13:07 +0700

    add new code

---
+++ assets/data/product.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    id: 1,
+    name: "Minyak Goreng Minyakita",
+    price: 15000,
+    image: "assets/images/minyak_kita.jpg",
+  },
+  {
+    id: 2,
+    name: "Susu Milkita Cokelat",
+    price: 2500,
+    image: "assets/images/susu_milkita.jpg",
+  },
+  {
+    id: 3,
+    name: "Beras Pulen 5kg",
+    price: 65000,
+    image: "assets/images/beras.jpg",
+  },
+  {
+    id: 4,
+    name: "Mie Instan Indomie",
+    price: 3000,
+    image: "assets/images/indomie.jpg",
+  },
+  // Tambahkan produk lain di sini
+];
